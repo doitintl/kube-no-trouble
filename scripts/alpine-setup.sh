041759a3848e04ca73b37417d782fe5aa2d1cf19
Stepan Stipl <stepan@stipl.net>
2020-04-09 17:34:14 +0100

    Fix upx - erroe with darwin build

--- scripts/alpine-setup.sh
+++ scripts/alpine-setup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
 
+UPX_VERSION="3.96"
+
 apk add --update --no-cache \
 	make \
-	tar \
-	upx
+	tar
+
+
+wget -qO- "https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-amd64_linux.tar.xz" \
+  | tar --strip 1 -xJv -C "/usr/local/bin/ upx-${UPX_VERSION}-amd64_linux/upx"
 
 go get github.com/rakyll/statik
