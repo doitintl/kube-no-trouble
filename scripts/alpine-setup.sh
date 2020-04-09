7cbadba968665b16113969b951cba6a7aa0e5156
Stepan Stipl <stepan@stipl.net>
2020-04-09 17:46:25 +0100

    Fix upx Alpine install

--- scripts/alpine-setup.sh
+++ scripts/alpine-setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# Set strict error checking
+set -emou pipefail
+LC_CTYPE=C
+
 UPX_VERSION="3.96"
 
 apk add --update --no-cache \
@@ -8,6 +12,6 @@ apk add --update --no-cache \
 
 
 wget -qO- "https://github.com/upx/upx/releases/download/v${UPX_VERSION}/upx-${UPX_VERSION}-amd64_linux.tar.xz" \
-  | tar --strip 1 -xJv -C "/usr/local/bin/ upx-${UPX_VERSION}-amd64_linux/upx"
+  | tar --strip 1 -xJv -C "/usr/local/bin/" "upx-${UPX_VERSION}-amd64_linux/upx"
 
 go get github.com/rakyll/statik
