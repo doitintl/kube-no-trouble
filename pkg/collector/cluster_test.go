2e8fc33f572b2a4114ca61f663dfbbdcbdd358bb
Stepan Stipl <stepan@stipl.net>
2020-09-04 22:36:52 +0100

    test: Add tests and fixtures for ClusterCollector -> Get
    fixes #35, #30

--- pkg/collector/cluster_test.go
+++ pkg/collector/cluster_test.go
@@ -2,7 +2,14 @@ package collector
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"testing"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/client-go/dynamic/fake"
+	"k8s.io/client-go/kubernetes/scheme"
 )
 
 func TestNewClusterCollectorBadPath(t *testing.T) {
@@ -31,5 +38,77 @@ func TestNewClusterCollectorValidEmptyCollector(t *testing.T) {
 
 	if err != nil && result != nil {
 		t.Errorf("Invalid schema")
+	}
+}
+
+func TestNewClusterCollectorFakeClient(t *testing.T) {
+	scheme := runtime.NewScheme()
+	clientset := fake.NewSimpleDynamicClient(scheme)
+	testOpts := ClusterOpts{ClientSet: clientset}
+
+	collector, err := NewClusterCollector(&testOpts)
+
+	if err != nil {
+		t.Errorf("failed to create cluster collector from fake client: %s", err)
+	}
+
+	result, err := collector.Get()
+
+	if err != nil || len(result) != 0 {
+		t.Errorf("expected to receive zero resources")
+	}
+}
+
+func TestClusterCollectorGetFake(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    []string // file list
+		expected int      // number of manifests
+	}{
+		{"empty", []string{}, 0},
+		{"withoutAnnotation", []string{"../../fixtures/fake-deployment-v1beta1-no-annotation.yaml"}, 0},
+		{"one", []string{"../../fixtures/fake-deployment-v1beta1-with-annotation.yaml"}, 1},
+		{"multiple", []string{"../../fixtures/fake-deployment-v1beta1-with-annotation.yaml", "../../fixtures/fake-ingress-v1beta1-with-annotation.yaml"}, 2},
+		{"mixed", []string{"../../fixtures/fake-deployment-v1beta1-no-annotation.yaml", "../../fixtures/fake-ingress-v1beta1-with-annotation.yaml"}, 1},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			//var objs []*unstructured.Unstructured
+			var objs []runtime.Object
+			for _, f := range tc.input {
+				obj := &unstructured.Unstructured{}
+
+				input, err := ioutil.ReadFile(f)
+				dec := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+				_, _, err = dec.Decode(input, nil, obj)
+				if err != nil {
+					t.Errorf("failed to decode input manifest %s: %s", f, err)
+				}
+				objs = append(objs, obj)
+			}
+
+			rscheme := runtime.NewScheme()
+			_ = scheme.AddToScheme(rscheme)
+
+			clientset := fake.NewSimpleDynamicClient(rscheme, objs...)
+			testOpts := ClusterOpts{ClientSet: clientset}
+
+			collector, err := NewClusterCollector(&testOpts)
+
+			if err != nil {
+				t.Errorf("failed to create collector from fake client: %s", err)
+			}
+
+			result, err := collector.Get()
+
+			if err != nil {
+				t.Errorf("expected to receive resources: %s", err)
+			}
+			if len(result) != tc.expected {
+				t.Errorf("expected to receive %d, received %d resources", tc.expected, len(result))
+			}
+		})
 	}
 }
