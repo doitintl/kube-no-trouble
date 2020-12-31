8703fdd428f8d8139b204920e5ffd0c7508cd137
Stepan Stipl <stepan@stipl.net>
2020-12-31 11:21:30 +0000

    feat: Add replace api version and since to text output
    Fixes #5

--- pkg/printer/text.go
+++ pkg/printer/text.go
@@ -46,9 +46,9 @@ func (c *TextPrinter) Print(results []judge.Result) error {
 			fmt.Fprintf(w, "%s\n", strings.Repeat("_", 90))
 			fmt.Fprintf(w, ">>> %s <<<\n", ruleSet)
 			fmt.Fprintf(w, "%s\n", strings.Repeat("-", 90))
-			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t\n", "KIND", "NAMESPACE", "NAME", "API_VERSION")
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s (%s)\n", "KIND", "NAMESPACE", "NAME", "API_VERSION", "REPLACE_WITH", "SINCE")
 		}
-		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t\n", r.Kind, r.Namespace, r.Name, r.ApiVersion)
+		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s (%s+)\n", r.Kind, r.Namespace, r.Name, r.ApiVersion, r.ReplaceWith, r.Since)
 	}
 	w.Flush()
 	return nil
