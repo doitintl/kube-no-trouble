47c707c485cdbed575b95fee18ec2a8af4e0e6bc
Stepan Stipl <stepan@stipl.net>
2021-06-14 13:35:15 +0100

    test: Refactor rules tests to avoid duplication

--- test/rules_122_test.go
+++ test/rules_122_test.go
@@ -2,16 +2,10 @@ package test
 
 import (
 	"testing"
-
-	"github.com/doitintl/kube-no-trouble/pkg/collector"
 )
 
 func TestRego122(t *testing.T) {
-	testCases := []struct {
-		name          string
-		manifests     []string
-		expectedKinds []string // kinds of objects
-	}{
+	testCases := []resourceFixtureTestCase{
 		{"ClusterRole", []string{"../fixtures/clusterrole-v1beta1.yaml"}, []string{"ClusterRole"}},
 		{"ClusterRoleBinding", []string{"../fixtures/clusterrolebinding-v1beta1.yaml"}, []string{"ClusterRoleBinding"}},
 		{"CSIDriver", []string{"../fixtures/csidriver-v1beta1.yaml"}, []string{"CSIDriver"}},
@@ -35,28 +29,5 @@ func TestRego122(t *testing.T) {
 		{"ValidatingWebhookConfiguration", []string{"../fixtures/validatingwebhookconfiguration-v1beta1.yaml"}, []string{"ValidatingWebhookConfiguration"}},
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			c, err := collector.NewFileCollector(
-				&collector.FileOpts{Filenames: tc.manifests},
-			)
-
-			if err != nil {
-				t.Errorf("Expected to succeed for %s, failed: %s", tc.manifests, err)
-			}
-
-			manifests, err := c.Get()
-			if err != nil {
-				t.Errorf("Expected to succeed for %s, failed: %s", tc.manifests, err)
-			} else if len(manifests) != len(tc.expectedKinds) {
-				t.Errorf("Expected to get %d, got %d", len(tc.expectedKinds), len(manifests))
-			}
-
-			for i := range manifests {
-				if manifests[i]["kind"] != tc.expectedKinds[i] {
-					t.Errorf("Expected to get %s, instead got: %s", tc.expectedKinds[i], manifests[i]["kind"])
-				}
-			}
-		})
-	}
+	testReourcesUsingFixtures(t, testCases)
 }
--- test/rules_125_test.go
+++ test/rules_125_test.go
@@ -2,41 +2,12 @@ package test
 
 import (
 	"testing"
-
-	"github.com/doitintl/kube-no-trouble/pkg/collector"
 )
 
 func TestRego125(t *testing.T) {
-	testCases := []struct {
-		name          string
-		manifests     []string
-		expectedKinds []string // kinds of objects
-	}{
+	testCases := []resourceFixtureTestCase{
 		{"RuntimeClass", []string{"../fixtures/runtimeclass-v1beta1.yaml"}, []string{"RuntimeClass"}},
 	}
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			c, err := collector.NewFileCollector(
-				&collector.FileOpts{Filenames: tc.manifests},
-			)
-
-			if err != nil {
-				t.Errorf("Expected to succeed for %s, failed: %s", tc.manifests, err)
-			}
-
-			manifests, err := c.Get()
-			if err != nil {
-				t.Errorf("Expected to succeed for %s, failed: %s", tc.manifests, err)
-			} else if len(manifests) != len(tc.expectedKinds) {
-				t.Errorf("Expected to get %d, got %d", len(tc.expectedKinds), len(manifests))
-			}
-
-			for i := range manifests {
-				if manifests[i]["kind"] != tc.expectedKinds[i] {
-					t.Errorf("Expected to get %s, instead got: %s", tc.expectedKinds[i], manifests[i]["kind"])
-				}
-			}
-		})
-	}
+	testReourcesUsingFixtures(t, testCases)
 }
