fabd7fba91579ba7556e1fb81d49ddcdb926a228
Stepan Stipl <stepan@stipl.net>
2021-06-16 11:12:31 +0100

    test: Add test for EndpointSlice

--- test/rules_125_test.go
+++ test/rules_125_test.go
@@ -9,6 +9,7 @@ func TestRego125(t *testing.T) {
 		{"RuntimeClass", []string{"../fixtures/runtimeclass-v1beta1.yaml"}, []string{"RuntimeClass"}},
 		{"PodDisruptionBudget", []string{"../fixtures/poddisruptionbudget-v1beta1.yaml"}, []string{"PodDisruptionBudget"}},
 		{"PodSecurityPolicy", []string{"../fixtures/podsecuritypolicy-v1beta1.yaml"}, []string{"PodSecurityPolicy"}},
+		{"EndpointSlice", []string{"../fixtures/endpointslice-v1beta1.yaml"}, []string{"EndpointSlice"}},
 	}
 
 	testReourcesUsingFixtures(t, testCases)
