2a7886711f55727f07b7ae9643809e60c77f261d
Stepan Stipl <stepan@stipl.net>
2021-06-16 10:12:07 +0100

    test: Add test for policy/v1beta1 - PodDisruptionBudget

--- test/rules_125_test.go
+++ test/rules_125_test.go
@@ -7,6 +7,7 @@ import (
 func TestRego125(t *testing.T) {
 	testCases := []resourceFixtureTestCase{
 		{"RuntimeClass", []string{"../fixtures/runtimeclass-v1beta1.yaml"}, []string{"RuntimeClass"}},
+		{"PodDisruptionBudget", []string{"../fixtures/poddisruptionbudget-v1beta1.yaml"}, []string{"PodDisruptionBudget"}},
 	}
 
 	testReourcesUsingFixtures(t, testCases)
