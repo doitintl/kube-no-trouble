a1127b5a323d0928726a424a48e38628249a5c24
Stepan Stipl <stepan@stipl.net>
2022-09-08 10:09:53 +0100

    test: Ref TestParsePrinter to test the resp. function
    And Test functions to follow Go naming conventions.

--- pkg/printer/printer_test.go
+++ pkg/printer/printer_test.go
@@ -1,23 +1,25 @@
 package printer
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestParsePrinters(t *testing.T) {
+func TestParsePrinter(t *testing.T) {
 	for k, v := range printers {
-		printer, err := NewPrinter(k)
+		p, err := ParsePrinter(k)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("failed to parse printer %s: %v", k, err)
 		}
-		if printer != v() {
-			t.Fatal("Should be a valid printer")
+
+		if reflect.ValueOf(p).Pointer() != reflect.ValueOf(v).Pointer() {
+			t.Fatalf("expected to get function %p, got %p instead", p, p)
 		}
 	}
 }
 
-func TestInvalidStringForParsePrinters(t *testing.T) {
-	_, err := NewPrinter("BAD")
+func TestParsePrinterInvalid(t *testing.T) {
+	_, err := ParsePrinter("BAD")
 	if err == nil {
 		t.Fatal(err)
 	}
