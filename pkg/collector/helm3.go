3324f9fcdbe2457c340ce078597147991b2e5fde
Myst <1592048+LeMyst@users.noreply.github.com>
2023-06-17 21:52:27 +0200

    feat: Add namespace support
    Add multi namespace support

--- pkg/collector/helm3.go
+++ pkg/collector/helm3.go
@@ -2,6 +2,7 @@ package collector
 
 import (
 	"github.com/rs/zerolog/log"
+	"helm.sh/helm/v3/pkg/release"
 	"helm.sh/helm/v3/pkg/storage"
 	"helm.sh/helm/v3/pkg/storage/driver"
 	"k8s.io/client-go/discovery"
@@ -13,9 +14,9 @@ const HELM_V3_COLLECTOR_NAME = "Helm v3"
 type HelmV3Collector struct {
 	*commonCollector
 	*kubeCollector
-	client       corev1.CoreV1Interface
-	secretsStore *storage.Storage
-	configStore  *storage.Storage
+	client        corev1.CoreV1Interface
+	secretsStores []*storage.Storage
+	configStores  []*storage.Storage
 }
 
 type HelmV3Opts struct {
@@ -25,7 +26,7 @@ type HelmV3Opts struct {
 	CoreClient      corev1.CoreV1Interface
 }
 
-func NewHelmV3Collector(opts *HelmV3Opts, userAgent string) (*HelmV3Collector, error) {
+func NewHelmV3Collector(opts *HelmV3Opts, namespaces []string, userAgent string) (*HelmV3Collector, error) {
 	kubeCollector, err := newKubeCollector(opts.Kubeconfig, opts.KubeContext, opts.DiscoveryClient, userAgent)
 	if err != nil {
 		return nil, err
@@ -42,27 +43,35 @@ func NewHelmV3Collector(opts *HelmV3Opts, userAgent string) (*HelmV3Collector, e
 		return nil, err
 	}
 
-	secretsDriver := driver.NewSecrets(collector.client.Secrets(""))
-	collector.secretsStore = storage.Init(secretsDriver)
+	for _, namespace := range namespaces {
+		secretsDriver := driver.NewSecrets(collector.client.Secrets(namespace))
+		collector.secretsStores = append(collector.secretsStores, storage.Init(secretsDriver))
 
-	configDriver := driver.NewConfigMaps(collector.client.ConfigMaps(""))
-	collector.configStore = storage.Init(configDriver)
+		configDriver := driver.NewConfigMaps(collector.client.ConfigMaps(namespace))
+		collector.configStores = append(collector.configStores, storage.Init(configDriver))
+	}
 
 	return collector, nil
 }
 
 func (c *HelmV3Collector) Get() ([]map[string]interface{}, error) {
-	releases, err := c.secretsStore.ListDeployed()
-	if err != nil {
-		return nil, err
+	var releases []*release.Release
+
+	for _, secretsStore := range c.secretsStores {
+		releasesSecret, err := secretsStore.ListDeployed()
+		if err != nil {
+			return nil, err
+		}
+		releases = append(releases, releasesSecret...)
 	}
 
-	releasesConfig, err := c.configStore.ListDeployed()
-	if err != nil {
-		return nil, err
+	for _, configStore := range c.configStores {
+		releasesConfig, err := configStore.ListDeployed()
+		if err != nil {
+			return nil, err
+		}
+		releases = append(releases, releasesConfig...)
 	}
-
-	releases = append(releases, releasesConfig...)
 
 	var results []map[string]interface{}
 
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -27,6 +27,7 @@ type Config struct {
 	AdditionalAnnotations []string
 	Cluster               bool
 	Context               string
+	Namespaces            []string
 	ExitError             bool
 	Filenames             []string
 	Helm3                 bool
@@ -54,6 +55,7 @@ func NewFromFlags() (*Config, error) {
 	flag.BoolVar(&config.Helm3, "helm3", true, "enable Helm v3 collector")
 	flag.StringSliceVarP(&config.Filenames, "filename", "f", []string{}, "manifests to check, use - for stdin")
 	flag.StringVarP(&config.Kubeconfig, "kubeconfig", "k", os.Getenv(clientcmd.RecommendedConfigPathEnvVar), "path to the kubeconfig file")
+	flag.StringSliceVarP(&config.Namespaces, "namespace", "n", []string{}, "namespaces to check (all by default)")
 	flag.StringVarP(&config.Output, "output", "o", TEXT, "output format - [text|json|csv]")
 	flag.StringVarP(&config.OutputFile, "output-file", "O", "-", "output file, use - for stdout")
 	flag.VarP(&config.LogLevel, "log-level", "l", "set log level (trace, debug, info, warn, error, fatal, panic, disabled)")
